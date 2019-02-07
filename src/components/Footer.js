464072cdc8a4a5dc63b1cf8e4e5e80f1f00c10c1
Leo Li <leoleodude@hotmail.com>
2019-02-06 19:27:09 -0800

    fixed up footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,27 @@
 import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
 
-const Footer = () => <div>This is the footer</div>;
+const styles = theme => ({
+  appBar: {
+    top: "auto",
+    bottom: "0",
+    margin: theme.spacing.unit
+  }
+});
 
-export default Footer;
+const Footer = () => (
+  <div>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="title" color="inherit">
+          React News App
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </div>
+);
+
+export default withStyles(styles)(Footer);
